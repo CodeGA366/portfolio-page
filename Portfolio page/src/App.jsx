2ad035325853f9075ff275e32be5c8983346dc0e
CodeGA366 <codega366@gmail.com>
2024-11-27 16:15:05 -0600

    added a button to the home page using the usNavigate hook, styled header/footer/home components

--- Portfolio page/src/App.jsx
+++ Portfolio page/src/App.jsx
@@ -1,6 +1,7 @@
 //import React from 'react' and router
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes, useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
 //import components
 import Header from './components/header.jsx';
 import HeroBanner from './components/HeroBanner';
@@ -10,31 +11,78 @@ import ContactMe from './components/ContactMe';
 import Resume from './components/Resume';
 import Footer from './components/Footer.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import './App.css'
+
+//defined styled component
+const HomeContainer = styled.div`
+    text-align: center;
+    padding: 250px;
+    background: linear-gradient(to bottom right, rgba(106, 27, 154, 0.8), rgba(37, 117, 252, 0.8)), url('../src/Images/pexels-goumbik-574070.jpg') no-repeat center center/cover;
+    color: white;
+
+    h1 {
+        font-size: 2.5rem;
+        color: #333;
+    }
+
+    p {
+        font-size: 1.2rem;
+        color: black;
+        margin: 20px 0;
+    }
+
+    @media (max-width: 600px) {
+        h1 {
+            font-size: 2rem;
+        }
+            
+        p {
+            font-size: 1rem;
+        }
+    }
+`;
+//styled button
+const CTAButton = styled.button`
+    background-color: #007bff;
+    color: white;
+    padding: 10px 20px;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+    font-size: 1rem;
+    margin-top: 20px;
+    
+    &:hover {
+        background-color: #0056b3;
+    }
+`;
 
 //App component
 const App = () => {
+    const navigate = useNavigate();
+
     return (
-        <Router>
-            <div>
-                <Header />
-                <HeroBanner />
+        <div id="root">
+            <Header />
+            <div className="main-content">
                 <Routes>
                     <Route path="/" element={
-                        <div>
+                        <HomeContainer>
                             <h1>Welcome to My Portfolio</h1>
-                            <p>This is the home page where you can find my latest projects and more about me</p>
-                            <p>Feel free to navigate to the other sections using the above menu!</p>
-                        </div>
+                            <p>This is the home page where you can find my latest projects and more about me!</p>
+                            <p>Feel free to navigate to the other sections using the above menu or the button below to view my work!</p>
+                            <CTAButton onClick={() => navigate('/work')}>View My Work</CTAButton>
+                        </HomeContainer>
                     } />
                     <Route path="/about-me" element={<AboutMe />} />
                     <Route path="/work" element={<Work />} />
                     <Route path="/contact-me" element={<ContactMe />} />
                     <Route path="/resume" element={<Resume />} />
                 </Routes>
-                <Footer />
             </div>
-        </Router>
+            <Footer />
+        </div>
     );
 };
 
--- Portfolio page/src/components/Header.jsx
+++ Portfolio page/src/components/Header.jsx
@@ -1,26 +1,32 @@
 //import react and link
 import React from 'react';
 import { Link } from 'react-router-dom';
+//import app.css
+import '../App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 //Header component
 const Header = () => {
     return (
-        <header className="bg-primary text-white p-3">
+        <header className="bg-custom text-white p-3">
             <nav className="navbar navbar-expand-lg">
                 <div className="container">
-                    <Link className="navbar-brand" to='/'>Home</Link>
-                    <div className="collapse navbar-collapse">
+                    <Link className="navbar-brand text-custom" to='/'>Home</Link>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label='Toggle navigation'>
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNav">
                         <ul className="navbar-nav">
                             <li className="nav-item">
-                                <Link className="nav-link" to='/about-me'>About Me</Link>
+                                <Link className="nav-link text-custom" to='/about-me'>About Me</Link>
                             </li>
                             <li className="nav-item">
-                                <Link className="nav-link" to='/work'>Portfollio</Link>
+                                <Link className="nav-link text-custom" to='/work'>Portfolio</Link>
                             </li>
                             <li className="nav-item">
-                                <Link className="nav-link" to='/contact-me'>Contact Me</Link>
+                                <Link className="nav-link text-custom" to='/contact-me'>Contact Me</Link>
                             </li>
-                            <li className="nav-item">
-                                <Link className="nav-link" to='/resume'>Resume</Link>
+                            <li className="nav-item text-custom">
+                                <Link className="nav-link text-custom" to='/resume'>Resume</Link>
                             </li>
                         </ul>
                     </div>
